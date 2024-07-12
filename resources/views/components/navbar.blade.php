669f1165be4951649da1112c96a3d735f4d19253
RasyaFajar123 <rasyafajar666@gmail.com>
2024-07-12 13:44:23 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<div class=" flex items-center w-full h-20 lg:px-28">
+    <div class="flex items-center w-1/6">
+        <h1 class="font-bold text-3xl">TASTY FOOD</h1>
+    </div>
+    <div class="flex items-center px-5 w-5/6 py-3 -mx-3">
+        <div class="flex items-center">
+            <a href="" class="mx-3 text-base">HOME</a>
+            <a href="" class="mx-3 text-base">TENTANG</a>
+            <a href="" class="mx-3 text-base">BERITA</a>
+            <a href="" class="mx-3 text-base">GALERI</a>
+            <a href="" class="mx-3 text-base">KONTAK</a>
+        </div>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner' : "url(/public/images/banner.png)"
+      }
+    },
+  },
+  plugins: [],
+}
